66b0d443623596af56ff02128caf343e9129efb5
Rose-Wei10 <weir0210@gmail.com>
2024-08-13 00:30:55 -0700

    first commit

---
+++ custom.d.ts
@@ -0,0 +1,10 @@
+// custom.d.ts
+declare namespace JSX {
+    interface IntrinsicElements {
+      'stripe-pricing-table': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        'pricing-table-id': string;
+        'publishable-key': string;
+      };
+    }
+  }
+  
